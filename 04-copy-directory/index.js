a84de190015953187c105d575218e0aba0b0ffbf
potasheva-n <mad.kyon@gmail.com>
2022-11-04 18:30:43 +0500

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const promise = require('fs/promises');
+
+fs.mkdir(`${__dirname}/files-copy`, { recursive: true }, error => {
+  if (error) {
+    throw error;
+  }
+});
+
+async function copyFilesInFolder () {
+  const files = await promise.readdir(`${__dirname}/files`, {withFileTypes: true});
+  files.map((file) => {
+    fs.copyFile(`${__dirname}/files/${file.name}`, `${__dirname}/files-copy/${file.name}`, err => {
+      if(err) throw err;
+   });
+  })
+}
+
+copyFilesInFolder();
 
